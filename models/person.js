699e49b8e754e9bd46519029e754f6af147b745f
Henri <henri.mynttinen@aalto.fi>
2018-02-12 20:38:40 +0200

    osa3.22

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://hemyntti1:RTtMVf0ryg@ds229008.mlab.com:29008/fullstack-hy-puhelinluettelo'
+const url = 'mongodb://hemyntti:<password>@ds229008.mlab.com:29008/fullstack-hy-puhelinluettelo'
 
 mongoose.connect(url)
 
